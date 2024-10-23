e0753d1d704edc9b8ec93a707d1debd5a77b47f1
kal1nadam <kalinaadam75@seznam.cz>
2024-10-23 15:12:59 +0200

    MainPage add FE tag filtering

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,23 +1,37 @@
 import { useEffect, useState } from "react"
 import { getQuotes } from "../api/api";
-import { Container } from "@mantine/core";
+import { Container, MultiSelect } from "@mantine/core";
 import { GetQuotesResponse, QuoteModel } from "../api/DTOs";
 import QuoteCard from "../components/QuoteCard";
 import { useAuth } from "../context/AuthContext";
+import { QuoteTagLabels, QuoteTagType } from "../enums/QuoteTagType";
 
 
 const MainPage = () => {
     const [quotes, setQuotes] = useState<QuoteModel[]>([]);
+    const [displayedQuotes, setDisplayedQuotes] = useState<QuoteModel[]>([]);
+    const [selectedTagFilters, setSelectedTagFilters] = useState<QuoteTagType[]>([]);
 
+
+    const fetchQuotes = async () => {
+        const response = await getQuotes();
+        const data: GetQuotesResponse = response.data;
+        setQuotes(data.quotes);
+        setDisplayedQuotes(data.quotes);
+    };
 
     useEffect(()=>{
-        const fetchQuotes = async () => {
-            const response = await getQuotes();
-            const data: GetQuotesResponse = response.data;
-            setQuotes(data.quotes);
-        };
         fetchQuotes();
     }, []);
+
+    // Filter
+    useEffect(() => {
+        // console.log(selectedTagFilters);
+        const filteredQuotes = quotes.filter((quote) => {
+            return selectedTagFilters.every((tag) => quote.tags.includes(tag));
+        });
+        setDisplayedQuotes(filteredQuotes);
+    }, [selectedTagFilters, quotes]);
 
     // Edit and Delete buttons will be shown only if the user is an admin
     const { isAdmin } = useAuth();
@@ -25,7 +39,20 @@ const MainPage = () => {
     return (
         <Container>
             <h1>Quotes</h1>
-            {quotes.map((quote) => (
+            <MultiSelect
+            label="Tag Filters"
+            mb={10}
+            data={Object.values(QuoteTagType).filter(v => typeof v === 'number').map((value) => ({
+                value: value.toString(),  // use string value for MultiSelect
+                label: QuoteTagLabels[value as QuoteTagType],  // display string value
+            }))}
+            value={selectedTagFilters.map((t) => t.toString())}
+            onChange={(values) => setSelectedTagFilters(values.map((v) => parseInt(v)))}
+            placeholder="Select tags"
+            searchable
+            clearable
+        />
+            {displayedQuotes.map((quote) => (
                 <QuoteCard
                     key={quote.id}
                     quote={quote}
